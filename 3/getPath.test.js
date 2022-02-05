d9183248a710cdda662ca389e0338e0d6884b406
Mikhail Khizhnyakov <64615735+khi-mik@users.noreply.github.com>
2022-02-05 19:33:31 +0300

    Update to initial version of test.js file
    lesson-3 - Homework

--- 3/getPath.test.js
+++ 3/getPath.test.js
@@ -1,20 +1,30 @@
 const getPath = require('./getPath');
 
-test('getPath for [9] Element should return an unique selector', () => {
-    const testElement = document.getElementsByTagName("*")[9];
-    expect(testElement).not.toBeUndefined();
 
+let elemIndex = 9;
+
+test('Searched element should not be undefined', () => {
+    const testElement = document.getElementsByTagName("*")[elemIndex];
+    expect(testElement).not.toBeUndefined();
+});
+
+test('Selector for the searched element should not be empty', () => {
+    const testElement = document.getElementsByTagName("*")[elemIndex];
     const selector = getPath(testElement);
     expect(selector).not.toBe('');
+});
 
+test('getPath() should return selector for an unique element', () => {
+    const testElement = document.getElementsByTagName("*")[elemIndex];
+    const selector = getPath(testElement);
     let count = document.querySelectorAll(selector).length;  
     expect(count).toBe(1);
 });
 
+
 [ 5, 8, 10, 14 ].forEach(
     function(item) {
-    
-        test(`getPath for [${item}] element should return an unique selector`, () => {
+        test(`getPath() for element[${item}] should return an unique selector`, () => {
             const testElement = document.getElementsByTagName("*")[item];
             expect(testElement).not.toBeUndefined();
         
@@ -24,6 +34,5 @@ test('getPath for [9] Element should return an unique selector', () => {
             let count = document.querySelectorAll(selector).length;  
             expect(count).toBe(1);
         });
-
     }
 );
